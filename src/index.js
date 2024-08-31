44141207de7296358519bca8bcb7a3635b00d95d
Matheus Guedes <matheusguedesmgs@gmail.com>
2024-08-31 17:40:09 -0300

    Correção de validação

--- src/index.js
+++ src/index.js
@@ -2,19 +2,18 @@ const todosOsInputs = document.querySelectorAll(".todos-inputs")
 const enviarFormulario = document.getElementById("enviar")
 const campoObrigatorio = document.querySelectorAll(".campo-obrigatorio-oculto")
 
-todosOsInputs.forEach((input, index)=>{
+enviarFormulario.addEventListener("click", ()=>{
 
-    enviarFormulario.addEventListener("click", ()=>{
-        if (input.value === ""){
-            todosOsInputs[index].classList.add("campo-nao-preenchido")
-            campoObrigatorio[index].classList.add("campo-obrigatorio-aparecendo")
+    todosOsInputs.forEach((input, index)=>{
 
-        } else {
+        if(input.value){
             todosOsInputs[index].classList.add("campo-preenchido")
-            campoObrigatorio[index].classList.remove("campo-obrigatorio-aparecendo")
+            todosOsInputs[index].classList.remove("campo-nao-preenchido")
 
-        }
+        }else {
+            todosOsInputs[index].classList.add("campo-nao-preenchido")
+            todosOsInputs[index].classList.remove("campo-preenchido")
+            campoObrigatorio[index].classList.add("campo-obrigatorio-aparecendo")
+        }      
     })
-
 })
-
